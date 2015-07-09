fc352e714144885ccdfcaba5a83da7474f036e91
jurgenizer <jurgen.geitner@gmail.com>
2015-07-09 13:30:12 +0200

    Added Django suit, updated 404.html to use css.

--- virtualmicroscope/settings.py
+++ virtualmicroscope/settings.py
@@ -24,12 +24,20 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
+
+#IMPORTANT for Django suit
+from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
+
+TEMPLATE_CONTEXT_PROCESSORS = TCP + (
+    'django.core.context_processors.request',
+)
 
 
 # Application definition
 
 INSTALLED_APPS = (
+    'suit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -85,3 +93,34 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 URL_ROOT = '/virtualmicroscope'
+
+# Django Suit configuration example
+SUIT_CONFIG = {
+    # header
+    'ADMIN_NAME': 'PLCVM Admin site',
+    'HEADER_DATE_FORMAT': 'l, j. F Y',
+    'HEADER_TIME_FORMAT': 'H:i',
+
+    # forms
+    'SHOW_REQUIRED_ASTERISK': True,  # Default True
+    'CONFIRM_UNSAVED_CHANGES': True, # Default True
+
+    # menu
+    # 'SEARCH_URL': '/admin/auth/user/',
+    'MENU_ICONS': {
+        'sites': 'icon-leaf',
+        'auth': 'icon-lock',
+     },
+     }
+    #'MENU_OPEN_FIRST_CHILD': True, # Default True
+    #'MENU_EXCLUDE': ('auth.group',),
+    #'MENU': (
+    #    'sites',
+    #    {'app': 'auth', 'icon':'icon-lock', 'models': ('user', 'group')},
+    #     {'label': 'Settings', 'icon':'icon-cog', 'models': ('auth.user', 'auth.group')},
+    #     {'label': 'Support', 'icon':'icon-question-sign', 'url': '/support/'},
+    # ),
+
+    # misc
+    # 'LIST_PER_PAGE': 15
+    #}
